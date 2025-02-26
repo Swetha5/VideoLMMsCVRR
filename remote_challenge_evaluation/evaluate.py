0b21d0922730cd00215052d65965ae14cacb5569
Swetha Sirnam <Swetha5@users.noreply.github.com>
2025-02-26 16:08:20 -0500

    Update evaluate.py

--- remote_challenge_evaluation/evaluate.py
+++ remote_challenge_evaluation/evaluate.py
@@ -1,8 +1,13 @@
 import random
+import json
+import openai
+import ast
+import time
+from multiprocessing.pool import Pool
 from pycocotools.coco import COCO
 from evaluation_script.cocoeval_mp import COCOevalMP
 
-def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
+def evaluate(test_annotation_file, user_submission_file, phase_codename, api_key, **kwargs):
     print("Starting Evaluation.....")
     """
     Evaluates the submission for a particular challenge phase and returns score
@@ -40,83 +45,127 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
             'submitted_at': u'2017-03-20T19:22:03.880652Z'
         }
     """
-    v3det_gt = COCO(test_annotation_file)  # gt annotation file
-    v3det_dt = v3det_gt.loadRes(user_submission_file)  # coco-format det results
-    v3det_eval = COCOevalMP(v3det_gt, v3det_dt, 'bbox', num_proc=8)
-    v3det_eval.params.maxDets = [300]
 
-    v3det_eval.evaluate()
-    v3det_eval.accumulate()
-    v3det_eval.summarize()
+    json_gt = json.load(open(test_annotation_file))[:5]
+    json_pred = json.load(open(user_submission_file))[:5]
+
+    data_dict = {}
+
+    for item in json_gt:
+        unique_key = f"{item['VideoID']}_{item['Q']}"
+        if unique_key  not in data_dict:
+            data_dict[unique_key] = {'a': item['A'], 'q': item['Q']}
+
+    for item in json_pred:
+        unique_key = f"{item['VideoID']}_{item['Q']}"
+        data_dict[unique_key] = {'pred': item['A']}
+
+    # Set the OpenAI API key.
+    openai.api_key = api_key
+    result_qa_pair = annotate(data_dict)
+
+    # v3det_gt = COCO(test_annotation_file)  # gt annotation file
+    # v3det_dt = v3det_gt.loadRes(user_submission_file)  # coco-format det results
+    # v3det_eval = COCOevalMP(v3det_gt, v3det_dt, 'bbox', num_proc=8)
+    # v3det_eval.params.maxDets = [300]
+
+    # v3det_eval.evaluate()
+    # v3det_eval.accumulate()
+    # v3det_eval.summarize()
+
+
     # output = {}
     # if phase_codename == "dev":
     #     print("Evaluating for Dev Phase")
     #     output["result"] = [
     #         {
-    #             "train_split": {
-    #                 "Metric1": random.randint(0, 99),
-    #                 "Metric2": random.randint(0, 99),
-    #                 "Metric3": random.randint(0, 99),
-    #                 "Total": random.randint(0, 99),
+    #             "OVD": {
+    #                 "bAP": random.randint(0, 99),
+    #                 "nAP": random.randint(0, 99),
+    #                 "AP": random.randint(0, 99),
     #             }
     #         }
     #     ]
-    #     # To display the results in the result file
-    #     output["submission_result"] = output["result"][0]["train_split"]
     #     print("Completed evaluation for Dev Phase")
     # elif phase_codename == "test":
     #     print("Evaluating for Test Phase")
     #     output["result"] = [
     #         {
-    #             "train_split": {
-    #                 "Metric1": random.randint(0, 99),
-    #                 "Metric2": random.randint(0, 99),
-    #                 "Metric3": random.randint(0, 99),
-    #                 "Total": random.randint(0, 99),
-    #             }
+    #             "split": "train_split",
+    #             "show_to_participant": True,
+    #             "accuracies": {"Metric1": 90},
     #         },
     #         {
-    #             "test_split": {
-    #                 "Metric1": random.randint(0, 99),
-    #                 "Metric2": random.randint(0, 99),
-    #                 "Metric3": random.randint(0, 99),
-    #                 "Total": random.randint(0, 99),
-    #             }
+    #             "split": "test_split",
+    #             "show_to_participant": False,
+    #             "accuracies": {"Metric1": 50, "Metric2": 40},
     #         },
     #     ]
-    #     # To display the results in the result file
-    #     output["submission_result"] = output["result"][0]
     #     print("Completed evaluation for Test Phase")
-    # output = dict()
-    # output['result'] = "empty"
-    # return output
+    return result_qa_pair
 
-    output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "OVD": {
-                    "bAP": random.randint(0, 99),
-                    "nAP": random.randint(0, 99),
-                    "AP": random.randint(0, 99),
-                }
-            }
-        ]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
-        output["result"] = [
-            {
-                "split": "train_split",
-                "show_to_participant": True,
-                "accuracies": {"Metric1": 90},
-            },
-            {
-                "split": "test_split",
-                "show_to_participant": False,
-                "accuracies": {"Metric1": 50, "Metric2": 40},
-            },
-        ]
-        print("Completed evaluation for Test Phase")
-    return output
+
+
+def annotate(prediction_set):
+    result_qa_pair = []
+    for ind, (k , single_dict) in enumerate(prediction_set.items()):
+        if ind > 10:
+            break
+        question = single_dict['q']
+        answer = single_dict['a']
+        pred = single_dict['pred']
+        while True:
+            try:
+                # Compute the correctness score
+                completion = openai.ChatCompletion.create(
+                    model="GPT-3.5-turbo-0125",
+                    messages=[
+                        {
+                            "role": "system",
+                            "content":
+                                "You are an intelligent chatbot designed for evaluating the correctness of AI assistant predictions for question-answer pairs. "
+                                "Your task is to compare the predicted answer with the ground-truth answer and determine if the predicted answer is correct or not. Here's how you can accomplish the task:"
+                                "------"
+                                "##INSTRUCTIONS: "
+                                "- Focus on the correctness and accuracy of the predicted answer with the ground-truth.\n"
+                                "- Consider predictions with less specific details as correct evaluation, unless such details are explicitly asked in the question.\n"
+                        },
+                        {
+                            "role": "user",
+                            "content":
+                                "Please evaluate the following video-based question-answer pair:\n\n"
+                                f"Question: {question}\n"
+                                f"Ground truth correct Answer: {answer}\n"
+                                f"Predicted Answer: {pred}\n\n"
+                                "Provide your evaluation as a correct/incorrect prediction along with the score where the score is an integer value between 0 (fully wrong) and 5 (fully correct). The middle score provides the percentage of correctness."
+                                "Please generate the response in the form of a Python dictionary string with keys 'pred', 'score' and 'reason', where value of 'pred' is  a string of 'correct' or 'incorrect', value of 'score' is in INTEGER, not STRING and value of 'reason' should providethe reason behind the decision."
+                                "Only provide the Python dictionary string."
+                                "For example, your response should look like this: {'pred': 'correct', 'score': 4.8, 'reason': reason}."
+                        }
+                    ]
+                )
+                # Convert response to a Python dictionary.
+                response_message = completion["choices"][0]["message"]["content"]
+                try:
+                    response_dict = ast.literal_eval(response_message)
+                except:
+                    # Remove the special characters.
+                    start_index = response_message.find("'reason': '") + len("'reason': '")
+                    end_index = response_message.find("'", start_index)
+
+                    # Extract the reason value
+                    reason_value = response_message[start_index:end_index]
+
+                    # Remove single quotes from the reason value
+                    reason_value = reason_value.replace("'", "")
+
+                    # Replace the original reason value with the modified one
+                    response_message = response_message[:start_index] + reason_value + '\'}'
+                    response_dict = ast.literal_eval(response_message)
+                result_qa_pair.append([response_dict, single_dict])
+                break
+            except Exception as e:
+                print(f"Error processing ': {e}")
+                time.sleep(60)
+
+    return result_qa_pair
